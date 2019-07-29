9855d8c0adf32f6f22fb63e7c68fc3db3b06d59f
cnlohr <lohr85@gmail.com>
2019-07-29 13:38:21 -0400

    bump

--- include/cnovrparts.h
+++ include/cnovrparts.h
@@ -32,15 +32,24 @@ typedef struct cnovr_rf_buffer_t
 cnovr_rf_buffer * CNOVRRFBufferCreate( int w, int h, int multisample );
 void CNOVRFBufferActivate( cnovr_rf_buffer * b );
 
-//////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 typedef struct cnovr_texture_t
 {
-	GLuint nTexture;
-	
+	cnovr_header header;
+	GLuint nTextureId;
+	uint8_t * data;
+	int width;
+	int height;
+	int channels;
+	const char * texfile;
 } cnovr_texture;
 
-//////////////////////////////////////////////////////////////////////////////
+cnovr_texture CNOVRTextureCreate( int initw, int inith, int initchan ); //Set to all 0 to have the load control these details.
+int CNOVRTextureLoadFile( cnovr_texture * tex, const char * texfile, int lod );
+int CNOVRTextureLoadData( cnovr_texture * tex, int w, int h, int chan, void * data, int lod );
+
+///////////////////////////////////////////////////////////////////////////////
 
 typedef struct cnovr_model_t
 {
