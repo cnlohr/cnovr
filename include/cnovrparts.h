bb7456be0102bf109026c5a316163609b03db874
cnlohr <lohr85@gmail.com>
2019-08-13 03:39:36 -0400

    thinking about a new way to do this.

--- include/cnovrparts.h
+++ include/cnovrparts.h
@@ -2,6 +2,7 @@
 #define _CNOVRPARTS_H
 
 #include <GL/gl.h>
+#include <os_generic.h>
 
 typedef int (*cnovrfn)( void * ths );
 
@@ -51,9 +52,50 @@ int CNOVRTextureLoadData( cnovr_texture * tex, int w, int h, int chan, void * da
 
 ///////////////////////////////////////////////////////////////////////////////
 
+typedef struct cnovr_geometry_t
+{
+	GLuint nIBO;
+	GLuint nVBO;
+
+	uint32_t iVertexCount;
+	uint32_t iIndexCount;
+
+	int * strides;
+	GLfloat * vertices; 
+	uint32_t * indices;
+
+	GLuint nRenderType;
+
+	uint8_t bTaint;
+	uint8_t nAllowCollisionMask;
+	uint8_t iStrides;
+	uint8_t bReserved;
+} cnovr_geometry;
+
+
+//XXX TODO: Consider wanting to have static data vs dynamic data as potentially separate arrays unlike how we're holding them here, interleved.
+cnovr_geometry * CreateGeo();
+void TackGeoVerts( int nverts, ... );
+void TackGeoIndices( int ninds, ... );
+void TackGeoVertsv( int nverts, float * v );
+void TackGeoIndicesv( int ninds, uint32_t * i );
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+
 typedef struct cnovr_model_t
 {
+	cnovr_header header;
+
+	cnovr_geometry * pGeos;
+	cnovr_texture * textures;
+	int iGeos;
+	int iTextures;
+	og_mutex_t model_mutex;
+	const char * geofile;
 } cnovr_model;
 
-#endif
+///////////////////////////////////////////////////////////////////////////////
+
 
