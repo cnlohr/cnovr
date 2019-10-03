bae8344aae1d30533f258473ee9f4dddeb32bab3
cnlohr <lohr85@gmail.com>
2019-10-03 04:48:34 -0400

    multithreaded reading

--- example_setup/dockablewindows.c
+++ example_setup/dockablewindows.c
@@ -11,15 +11,6 @@
 #include <X11/extensions/XShm.h>
 #include <errno.h>
 
-extern XWindowAttributes CNFGWinAtt;
-extern XClassHint *CNFGClassHint;
-extern Display *CNFGDisplay;
-extern Window CNFGWindow;
-extern Pixmap CNFGPixmap;
-extern GC     CNFGGC;
-extern GC     CNFGWindowGC;
-extern Visual * CNFGVisual;
-
 int handle;
 
 cnovr_model * model;
@@ -27,85 +18,153 @@ cnovr_shader * shader;
 cnovr_texture * texture;
 cnovr_pose pose;
 
-Window windowtrack;
 
+/////////////////////////////////////////////////////////////////////////////////
+// Display stuff
+
+og_thread_t gtt;
+
+int need_texture = 1;
+Window windowtrack;
 static XImage *image;
 XShmSegmentInfo shminfo;
 int width, height;
+Display * localdisplay;
+int quitting;
 
 
-
-int handler(Display * d, XErrorEvent * e)
+int Xerrhandler(Display * d, XErrorEvent * e)
 {
 	printf( "XShmGetImage error\n" );
     return 0;
 }
 
-void PreRender()
+
+void ListWindows()
 {
-	if( windowtrack )
+	double start = OGGetAbsoluteTime();
+
+    bool found = false;
+    Window rootWindow = RootWindow( localdisplay, DefaultScreen(localdisplay));
+    Atom atom = XInternAtom(localdisplay, "_NET_CLIENT_LIST", true);
+    Atom actualType;
+    int format;
+    unsigned long numItems;
+    unsigned long bytesAfter;
+    
+    unsigned char *data = '\0';
+    Window *list;    
+    char *windowName;
+
+    int status = XGetWindowProperty(localdisplay, rootWindow, atom, 0L, (~0L), false,
+        AnyPropertyType, &actualType, &format, &numItems, &bytesAfter, &data);
+    list = (Window *)data;
+    
+    if (status >= Success && numItems) {
+        for (int i = 0; i < numItems; ++i) {
+            status = XFetchName(localdisplay, list[i], &windowName);
+            if (status >= Success) {
+				if( strstr( windowName, "Firefox" ) != 0 )
+				{
+					windowtrack = list[i];
+					found = 1;
+					break;
+				}
+            }
+        }
+    }
+    XFree(windowName);
+    XFree(data);
+
+	if( found )
 	{
 		XWindowAttributes attribs;
-	    XGetWindowAttributes(CNFGDisplay, windowtrack, &attribs);
-		int taint = 0;
-		if( attribs.width != width ) taint = 1;
-		if( attribs.height != height ) taint = 1;
-	    width = attribs.width;
-	    height = attribs.height;
-	//	printf( "%d %d\n", width, height );
+	    XGetWindowAttributes(localdisplay, windowtrack, &attribs);
 
-		if( !image )
+	    int width = attribs.width;
+	    int height = attribs.height;
+		printf( "%d %d\n", width, height );
+	}
+	else
+	{
+		printf( "Window not found\n" );
+	}
+
+	printf( "Time spent: %f\n", OGGetAbsoluteTime() - start );
+}
+
+
+void * GetTextureThread( void * v )
+{
+	XInitThreads();
+	localdisplay = XOpenDisplay(NULL);
+	XSetErrorHandler(Xerrhandler);
+	ListWindows();
+
+	while( !quitting )
+	{
+		if( windowtrack && need_texture == 1 )
 		{
-			image = XShmCreateImage( CNFGDisplay, 
-				attribs.visual, attribs.depth,
-				ZPixmap, NULL, &shminfo, width, height); 
+			XWindowAttributes attribs;
+			XGetWindowAttributes(localdisplay, windowtrack, &attribs);
+			int taint = 0;
+			if( attribs.width != width ) taint = 1;
+			if( attribs.height != height ) taint = 1;
+			width = attribs.width;
+			height = attribs.height;
+		//	printf( "%d %d\n", width, height );
 
-			shminfo.shmid = shmget(IPC_PRIVATE,
-				image->bytes_per_line * image->height,
-				IPC_PRIVATE | IPC_EXCL | IPC_CREAT|0777);
-
-			printf( "%d ERRNO %d %d %d\n", shminfo.shmid, errno, image->bytes_per_line, image->height );
-
-			if( shminfo.shmid > 0 )
+			if( shminfo.shmid <= 0 )
 			{
+
+				printf( "getting SHM\n" );
+				shminfo.shmid = shmget(IPC_PRIVATE,
+					2048*4 * 2048,
+					IPC_PRIVATE | IPC_EXCL | IPC_CREAT|0777);
+				printf( "%d ERRNO %d %d %d    %p\n", shminfo.shmid, errno, image );
+			}
+
+			if( taint  || !image)
+			{
+				image = XShmCreateImage( localdisplay, 
+					attribs.visual, attribs.depth,
+					ZPixmap, NULL, &shminfo, width, height); 
+
 				shminfo.shmaddr = image->data = shmat(shminfo.shmid, 0, 0);
 				shminfo.readOnly = False;
-				XShmAttach(CNFGDisplay, &shminfo);
+				printf( "Attaching\n" );
+				XShmAttach(localdisplay, &shminfo);
 			}
-		}
-		else
-		if(taint )
-		{
-			printf( "TAINT!!\n" );
-			image = XShmCreateImage( CNFGDisplay, 
-				attribs.visual, attribs.depth,
-				ZPixmap, NULL, &shminfo, width, height); 
-			shminfo.shmid = shmget(IPC_PRIVATE,
-				image->bytes_per_line * image->height,
-				IPC_CREAT|0777);
-
+	 
 			if( shminfo.shmid > 0 )
 			{
-				shminfo.shmaddr = image->data = shmat(shminfo.shmid, 0, 0);
-				shminfo.readOnly = False;
-				XShmAttach(CNFGDisplay, &shminfo);
+				double ds = OGGetAbsoluteTime();
+				XShmGetImage(localdisplay, windowtrack, image, 0, 0, AllPlanes);
+				double part1 = OGGetAbsoluteTime() - ds;
+				if( image && image->data && (void*)(&(image->data[0])) != (void*)(-1) )
+				{
+					need_texture = 0;
+				}
+				if( part1 > .0015 )
+					printf( "GET %10f\n", part1 );
 			}
 		}
+		OGUSleep( 1000 );
+	}
+	return 0;
+}
 
 
-		if( shminfo.shmid > 0 )
-		{
-			//XSetErrorHandler(handler);
-			XShmGetImage(CNFGDisplay, windowtrack, image, 0, 0, AllPlanes);
-			//XSetErrorHandler(0);
-		//	printf( "%p\n", (void*)(&(image->data[0])) );
-			if( image && image->data && (void*)(&(image->data[0])) != (void*)(-1) )
-			{
-				printf( "A %p %d %d %p\n", texture, image->width, image->height, (void*)(&(image->data[0])) );
-				CNOVRTextureLoadDataNow( texture, image->width, image->height, 4, 0,  (void*)(&(image->data[0])), 1 );
-				printf( "B\n" );
-			}
-		}
+
+void PreRender()
+{
+	if( !need_texture )
+	{
+		double ds = OGGetAbsoluteTime();
+		CNOVRTextureLoadDataNow( texture, image->width, image->height, 4, 0,  (void*)(&(image->data[0])), 1 );
+		double part1 = OGGetAbsoluteTime() - ds;
+		if( part1 > 0.015 ) printf( "Loading Data %f\n", part1 );
+		need_texture = 1;
 	}
 }
 
@@ -129,59 +188,6 @@ void GenGeo()
 	texture = CNOVRTextureCreate( 0, 0, 0 ); 
 }
 
-void ListWindows()
-{
-	double start = OGGetAbsoluteTime();
-
-    bool found = false;
-    Window rootWindow = RootWindow( CNFGDisplay, DefaultScreen(CNFGDisplay));
-    Atom atom = XInternAtom(CNFGDisplay, "_NET_CLIENT_LIST", true);
-    Atom actualType;
-    int format;
-    unsigned long numItems;
-    unsigned long bytesAfter;
-    
-    unsigned char *data = '\0';
-    Window *list;    
-    char *windowName;
-
-    int status = XGetWindowProperty(CNFGDisplay, rootWindow, atom, 0L, (~0L), false,
-        AnyPropertyType, &actualType, &format, &numItems, &bytesAfter, &data);
-    list = (Window *)data;
-    
-    if (status >= Success && numItems) {
-        for (int i = 0; i < numItems; ++i) {
-            status = XFetchName(CNFGDisplay, list[i], &windowName);
-            if (status >= Success) {
-				if( strstr( windowName, "Firefox" ) != 0 )
-				{
-					windowtrack = list[i];
-					found = 1;
-					break;
-				}
-            }
-        }
-    }
-    XFree(windowName);
-    XFree(data);
-
-	if( found )
-	{
-		XWindowAttributes attribs;
-	    XGetWindowAttributes(CNFGDisplay, windowtrack, &attribs);
-
-	    int width = attribs.width;
-	    int height = attribs.height;
-		printf( "%d %d\n", width, height );
-	}
-	else
-	{
-		printf( "Window not found\n" );
-	}
-
-	printf( "Time spent: %f\n", OGGetAbsoluteTime() - start );
-}
-
 
 void prerender_startup( void * tag, void * opaquev )
 {
@@ -196,13 +202,20 @@ void start( const char * identifier )
 	printf( "Dockables start %s(%p)\n", identifier, identifier );
 
 	CNOVRJobTack( cnovrQPrerender, prerender_startup, 0, 0, 0 );
-
+	gtt = OGCreateThread( GetTextureThread, 0 );
 	printf( "Dockables start OK %s\n", identifier );
+
 }
 
 void stop( const char * identifier )
 {
-	CNOVRListAdd( cnovrLRender, &handle, Render );
+	printf( "Stopping\n" );
+	quitting = 1;
+	XCloseDisplay( localdisplay );
+	printf( "Joining\n" );
+	OGJoinThread( gtt );
+	printf( "Stopped\n" );
+
 	CNOVRListDeleteTag( &handle );
 	printf( "Dockables Destroying: %p %p\n",shader, model );
 	CNOVRDelete( shader );
